f1133429a21b5ec72295abcb84759f86910160f1
pdev6373 <pdev6373@gmail.com>
2023-09-23 10:08:41 +0100

    work on the registeration for the user role

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -42,126 +42,117 @@ const getAllUsers = asyncHandler(async (req, res) => {
 const createNewUser = asyncHandler(async (req, res) => {
   const { role } = req.body;
 
-  // User should have one role -> User or Company
-  if (!role)
-    return res
-      .status(403)
-      .json({ success: false, message: "All fields are required" });
-  else if (typeof role !== "string")
-    return res
-      .status(403)
-      .json({ success: false, message: "Invalid role type" });
-  else if (role == "User") {
-    const { email, password } = req.body;
+  // User should have only one role -> User or Company
+  switch (role) {
+    case "User": {
+      const { email, password } = req.body;
 
-    // Validate entries
-    if (!email || !password)
+      // Validate entries
+      if (!email || !password)
+        return res
+          .status(403)
+          .json({ success: false, message: "All fields are required" });
+
+      // check if user exists
+      const user = await User.findOne({ email }).exec();
+
+      // user exist
+      if (user) {
+        // user is verified
+        if (user.isVerified)
+          return res
+            .status(409)
+            .json({ success: false, message: "User is already registered" });
+
+        // user not verified
+        user.role = role;
+        user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        const sendOTPResponse = await sendOTP(email);
+
+        return res.json(sendOTPResponse);
+      }
+
+      //  user does not exist
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const userObject = {
+        email,
+        password: hashedPassword,
+        role,
+      };
+
+      const isUserRegisterd = await User.create(userObject);
+
+      if (isUserRegisterd) {
+        const sendOTPResponse = await sendOTP(email);
+        return res.json(sendOTPResponse);
+      }
+
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user data received" });
+    }
+
+    case "Company":
+      return res.status(403).json({ success: false, message: "Coming soon.." });
+
+    default:
       return res
         .status(403)
-        .json({ success: false, message: "All fields are required" });
-
-    // check if user exists
-    const user = await User.findOne({ email }).exec();
-
-    // user exist
-    if (user) {
-      // user is verified
-      if (user.isVerified)
-        return res
-          .status(409)
-          .json({ success: false, message: "User is already registered" });
-
-      // user not verified
-      user.role = role;
-      user.password = await bcrypt.hash(password, 10);
-
-      await user.save();
-      const sendOTPResponse = await sendOTP(email);
-
-      return res.json(sendOTPResponse);
-    }
-
-    //  user does not exist
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const userObject = {
-      email,
-      password: hashedPassword,
-      role,
-    };
-
-    const isUserRegisterd = await User.create(userObject);
-
-    if (isUserRegisterd) {
-      const sendOTPResponse = await sendOTP(email);
-      return res.json(sendOTPResponse);
-    }
-
-    res
-      .status(400)
-      .json({ success: false, message: "Invalid user data received" });
-  } else if (role === "Company") {
-    return res.status(403).json({ success: false, message: "Coming soon.." });
-  } else {
-    return res
-      .status(403)
-      .json({ success: false, message: "Invalid user role" });
+        .json({ success: false, message: "Invalid user role" });
   }
-
-  res
-    .status(400)
-    .json({ success: false, message: "Invalid user data received" });
 });
 
 const updateUser = asyncHandler(async (req, res) => {
   const { role } = req.body;
 
-  if (!role)
-    return res
-      .status(403)
-      .json({ success: false, message: "All fields are required" });
-  else if (typeof role !== "string")
-    return res
-      .status(403)
-      .json({ success: false, message: "Invalid role type" });
-  if (role == "User") {
-    const { id, email, password } = req.body;
+  switch (role) {
+    case "User": {
+      const { id, email, password } = req.body;
 
-    if (!id || !email)
+      if (!id || !email)
+        return res
+          .status(403)
+          .json({ success: false, message: "All fields are required" });
+
+      const user = await User.findById(id).exec();
+
+      if (!user)
+        return res
+          .status(400)
+          .json({ success: false, message: "User not found" });
+
+      if (!user.isVerified)
+        return res
+          .status(403)
+          .json({ success: false, message: "User not verified" });
+
+      const duplicate = await User.findOne({ email }).lean().exec();
+      if (duplicate && duplicate?._id.toString() !== id)
+        return res
+          .status(409)
+          .json({ success: false, message: "Duplicate email" });
+
+      user.email = email;
+      user.role = role;
+
+      if (password) user.password = await bcrypt.hash(password, 10);
+      const updatedUser = await user.save();
+
+      return res.json({
+        success: true,
+        message: `${updatedUser.email} updated`,
+      });
+    }
+
+    case "Company":
+      return res.status(403).json({ success: false, message: "Coming soon.." });
+
+    default:
       return res
         .status(403)
-        .json({ success: false, message: "All fields are required" });
-
-    const user = await User.findById(id).exec();
-
-    if (!user)
-      return res
-        .status(400)
-        .json({ success: false, message: "User not found" });
-
-    if (!user.isVerified)
-      return res
-        .status(403)
-        .json({ success: false, message: "User not verified" });
-
-    const duplicate = await User.findOne({ email }).lean().exec();
-    if (duplicate && duplicate?._id.toString() !== id)
-      return res
-        .status(409)
-        .json({ success: false, message: "Duplicate email" });
-
-    user.email = email;
-    user.role = role;
-
-    if (password) user.password = await bcrypt.hash(password, 10);
-    const updatedUser = await user.save();
-
-    res.json({ success: true, message: `${updatedUser.email} updated` });
-  } else if (role === "Company") {
-    return res.status(403).json({ success: false, message: "Coming soon.." });
-  } else {
-    return res
-      .status(403)
-      .json({ success: false, message: "Invalid user role" });
+        .json({ success: false, message: "Invalid user role" });
   }
 });
 
